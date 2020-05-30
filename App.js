2422048a4c70c4be1ef0d129a0f536408db6f58e
yufias <yupi41@gmail.com>
2020-05-30 19:08:43 +0700

    add icons for drawer navigation

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import 'react-native-gesture-handler'
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { Ionicons } from 'react-native-vector-icons'
 import LandingPage from './components/LandingPage'
 import Account from './components/Account'
 
@@ -13,7 +14,25 @@ export default function App() {
     // <View style={styles.container}>
       <NavigationContainer>
       {/* <Text>Home</Text> */}
-        <Tab.Navigator>
+        <Tab.Navigator
+          screenOptions={({ route }) => ({
+            tabBarIcon: ({ color, size }) => {
+              let iconName;
+
+              if(route.name === 'Home') {
+                iconName = 'md-home'
+              } else if(route.name === 'Account') {
+                iconName = 'md-contact'
+              }
+
+              return <Ionicons name={iconName} size={size} color={color} />
+            },
+          })}
+          tabBarOptions={{
+            activeTintColor: '#6836bb',
+            inactiveTintColor: 'gray'
+          }}
+        >
           <Tab.Screen name="Home" component={LandingPage} />
           <Tab.Screen name="Account" component={Account} />
         </Tab.Navigator>
